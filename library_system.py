ac086e0661e13e1587adec923f0aaec646ae2297
Nguyen Kim Cuong <cuongnk@thinhtientech.com>
2022-11-24 23:41:49 +0700

    Convert to object-oriented application

--- library_system.py
+++ library_system.py
@@ -126,140 +126,141 @@ from api import *
 #     btn_check = tk.Button(editwindow, text='Kiểm tra thẻ', command=checklc_button)
 #     btn_check.place(x=170, y=50)
 
+class AboutFrame(tk.Frame):
+    def __init__(self, parent, controller):
+
+        tk.Frame.__init__(self, parent)
+
+        label = ttk.Label(self, text='Author: Nguyễn Kim Cương')
+        label.pack(ipadx=10, ipady=10)
+
+class BookFrame(tk.Frame):
+    def __init__(self, parent, controller):
+
+        tk.Frame.__init__(self, parent)
+        # Configure frame
+        self.columnconfigure(0, weight=1)
+        self.columnconfigure(1, weight=3)
+        self.rowconfigure(0, weight=1)
+
+        print(controller)
+
+        book_label = ['Mã sách', 'Tên sách', 'Mã NXB', 'Mã tác giả', 'Mã TLS', 'Mã vị trí', 'Năm XB', 'SL tổng', 'SL tồn kho', 'Đơn giá']
+        book_var = [tk.StringVar()] * len(book_label)
+
+        # Set variable values
+        # for i in range(len(book_label)):
+        #     book_var[i].set('placeholder')
+
+        # Left menu
+        menu_left = tk.Frame(self, highlightbackground='#ababab', highlightthickness=1)
+        menu_left.columnconfigure(0, weight=1)
+        menu_left.rowconfigure(0, weight=1)
+        menu_left.rowconfigure(1, weight=0)
+
+        menu_left.grid(column=0, row=0, sticky=tk.NSEW, padx=(10, 0), pady=10)
+
+        # Upper left menu
+        menu_upper_left = tk.Frame(menu_left)
+        menu_upper_left.columnconfigure(0, weight=1)
+        menu_upper_left.columnconfigure(1, weight=2)
+        menu_upper_left.rowconfigure(10, weight=1)
+        
+        menu_upper_left.grid(column=0, row=0, sticky=tk.NSEW)
+
+        label_arr = []
+        entry_arr = []
+        for i in range(len(book_label)):
+            label_arr.append(ttk.Label(menu_upper_left, text=book_label[i]))
+            label_arr[i].grid(column=0, row=i, sticky=tk.W, padx=(10, 0), pady=10)
+            entry_arr.append(ttk.Entry(menu_upper_left, textvariable=book_var[i]))
+            entry_arr[i].grid(column=1, row=i, sticky=tk.EW, padx=(0, 10), pady=10)
+
+        # Lower left menu
+        menu_lower_left = tk.Frame(menu_left)
+        menu_lower_left.columnconfigure(0, weight=1)
+        menu_lower_left.columnconfigure(1, weight=1)
+        menu_lower_left.rowconfigure(0, weight=1)
+        menu_lower_left.rowconfigure(1, weight=1)
+
+        menu_lower_left.grid(column=0, row=1, sticky=tk.NSEW)
+
+        def button_clicked():
+            print('clicked')
+            button.state(['disabled'])
+
+        button = ttk.Button(menu_lower_left, text='Tìm', compound=tk.LEFT, command=button_clicked)
+        button.grid(column=0, row=0, sticky=tk.NSEW, padx=10, pady=10)
+
+        button = ttk.Button(menu_lower_left, text='Thêm', compound=tk.LEFT, command=button_clicked)
+        button.grid(column=1, row=0, sticky=tk.NSEW, padx=10, pady=10)
+
+        button = ttk.Button(menu_lower_left, text='Sửa', compound=tk.LEFT, command=button_clicked)
+        button.grid(column=0, row=1, sticky=tk.NSEW, padx=10, pady=10)
+
+        button = ttk.Button(menu_lower_left, text='Xóa', compound=tk.LEFT, command=button_clicked)
+        button.grid(column=1, row=1, sticky=tk.NSEW, padx=10, pady=10)
+
+        # Right menu
+        menu_right = tk.Frame(self, highlightbackground='#ababab', highlightthickness=1)
+        menu_right.columnconfigure(0, weight=1)
+        menu_right.columnconfigure(1, weight=0)
+        menu_right.rowconfigure(0, weight=1)
+
+        menu_right.grid(column=1, row=0, sticky=tk.NSEW, padx=10, pady=10)
+
+        text = tk.Text(menu_right, height=10)
+        text.grid(column=0, row=0, sticky=tk.NSEW)
+
+        scrollbar = ttk.Scrollbar(menu_right, orient='vertical', command=text.yview)
+        scrollbar.grid(column=1, row=0, sticky=tk.NS)
+
+        text['yscrollcommand'] = scrollbar.set
+
+        for i in range(1,50):
+            position = f'{i}.0'
+            text.insert(position,f'Line {i}\n')
+
+    # class book_label:
+    #     id = 'Mã sách'
+    #     name = 'Tên sách'
+    #     publisher_id = 'Mã NXB'
+    #     author_id = 'Mã tác giả'
+    #     category_id = 'Mã TLS'
+    #     position_id = 'Mã vị trí'
+    #     published_year = 'Năm XB'
+    #     total_amount = 'SL tổng'
+    #     available_amount = 'SL tồn kho'
+    #     price = 'Đơn giá'
+
 class App(tk.Tk):
     def __init__(self):
+
         super().__init__()
 
         def no_func():
             print('Nothing here')
-
-        def hide_all_frames():
-            for widget in about_frame.winfo_children():
-                widget.destroy()
-            for widget in book_frame.winfo_children():
-                widget.destroy()
-            about_frame.pack_forget()
-            book_frame.pack_forget()
-
-        # About
-        about_frame = ttk.Frame(self)
-
-        def about():
-            hide_all_frames()
-            about_frame.pack(fill="both", expand=1)
-            create_about_widgets()
-
-        def create_about_widgets():
-            label = ttk.Label(about_frame, text='Author: Nguyễn Kim Cương')
-            label.pack(ipadx=0, ipady=20)
-
-        # Book
-        book_frame = ttk.Frame(self)
-        book_frame.columnconfigure(0, weight=1)
-        book_frame.columnconfigure(1, weight=3)
-        book_frame.rowconfigure(0, weight=1)
-        
-        book_label = ['Mã sách', 'Tên sách', 'Mã NXB', 'Mã tác giả', 'Mã TLS', 'Mã vị trí', 'Năm XB', 'SL tổng', 'SL tồn kho', 'Đơn giá']
-        book_var = []
-        for i in range(len(book_label)):
-            book_var.append(tk.StringVar())
-
-        # class book_label:
-        #     id = 'Mã sách'
-        #     name = 'Tên sách'
-        #     publisher_id = 'Mã NXB'
-        #     author_id = 'Mã tác giả'
-        #     category_id = 'Mã TLS'
-        #     position_id = 'Mã vị trí'
-        #     published_year = 'Năm XB'
-        #     total_amount = 'SL tổng'
-        #     available_amount = 'SL tồn kho'
-        #     price = 'Đơn giá'
-
-        def manage_book():
-            hide_all_frames()
-            book_frame.pack(fill="both", expand=1)
-            create_book_widgets()
-
-        def create_book_widgets():
-            # Set variable values
-            # for i in range(len(book_label)):
-            #     book_var[i].set('placeholder')
-
-            # Left menu
-            menu_left = tk.Frame(book_frame, highlightbackground='#ababab', highlightthickness=1)
-            menu_left.columnconfigure(0, weight=1)
-            menu_left.rowconfigure(0, weight=1)
-            menu_left.rowconfigure(1, weight=0)
-
-            menu_left.grid(column=0, row=0, sticky=tk.NSEW, padx=(10, 0), pady=10)
-
-            # Upper left menu
-            menu_upper_left = tk.Frame(menu_left)
-            menu_upper_left.columnconfigure(0, weight=1)
-            menu_upper_left.columnconfigure(1, weight=2)
-            menu_upper_left.rowconfigure(10, weight=1)
-            
-            menu_upper_left.grid(column=0, row=0, sticky=tk.NSEW)
-
-            label_arr = []
-            entry_arr = []
-            for i in range(len(book_label)):
-                label_arr.append(ttk.Label(menu_upper_left, text=book_label[i]))
-                label_arr[i].grid(column=0, row=i, sticky=tk.W, padx=(10, 0), pady=10)
-                entry_arr.append(ttk.Entry(menu_upper_left, textvariable=book_var[i]))
-                entry_arr[i].grid(column=1, row=i, sticky=tk.EW, padx=(0, 10), pady=10)
-
-            # Lower left menu
-            menu_lower_left = tk.Frame(menu_left)
-            menu_lower_left.columnconfigure(0, weight=1)
-            menu_lower_left.columnconfigure(1, weight=1)
-            menu_lower_left.rowconfigure(0, weight=1)
-            menu_lower_left.rowconfigure(1, weight=1)
-
-            menu_lower_left.grid(column=0, row=1, sticky=tk.NSEW)
-
-            def button_clicked():
-                print('clicked')
-                button.state(['disabled'])
-
-            button = ttk.Button(menu_lower_left, text='Tìm', compound=tk.LEFT, command=button_clicked)
-            button.grid(column=0, row=0, sticky=tk.NSEW, padx=10, pady=10)
-
-            button = ttk.Button(menu_lower_left, text='Thêm', compound=tk.LEFT, command=button_clicked)
-            button.grid(column=1, row=0, sticky=tk.NSEW, padx=10, pady=10)
-
-            button = ttk.Button(menu_lower_left, text='Sửa', compound=tk.LEFT, command=button_clicked)
-            button.grid(column=0, row=1, sticky=tk.NSEW, padx=10, pady=10)
-
-            button = ttk.Button(menu_lower_left, text='Xóa', compound=tk.LEFT, command=button_clicked)
-            button.grid(column=1, row=1, sticky=tk.NSEW, padx=10, pady=10)
-
-            # Right menu
-            menu_right = tk.Frame(book_frame, highlightbackground='#ababab', highlightthickness=1)
-            menu_right.columnconfigure(0, weight=1)
-            menu_right.columnconfigure(1, weight=0)
-            menu_right.rowconfigure(0, weight=1)
-
-            menu_right.grid(column=1, row=0, sticky=tk.NSEW, padx=10, pady=10)
-
-            text = tk.Text(menu_right, height=10)
-            text.grid(column=0, row=0, sticky=tk.NSEW)
-
-            scrollbar = ttk.Scrollbar(menu_right, orient='vertical', command=text.yview)
-            scrollbar.grid(column=1, row=0, sticky=tk.NS)
-
-            text['yscrollcommand'] = scrollbar.set
-
-            for i in range(1,50):
-                position = f'{i}.0'
-                text.insert(position,f'Line {i}\n')
 
         # Root window
         self.title('Hệ thống quản lý thư viện')
         self.geometry('1280x720')
         # self.resizable(False, False)
         self.iconbitmap('./assets/book.ico')
+
+        # Container frame
+        container = tk.Frame(self)
+        container.pack(side='top', fill='both', expand=True)
+        container.rowconfigure(0, weight=1)
+        container.columnconfigure(0, weight=1)
+
+        self.frames = {}
+        for F in (AboutFrame, BookFrame):
+            frame = F(container, self)
+            self.frames[F] = frame
+            frame.grid(column=0, row=0, sticky=tk.NSEW)
+
+        # Default frame
+        self.show_frame(AboutFrame)
 
         # Create a menubar
         menubar = tk.Menu(self)
@@ -270,7 +271,7 @@ class App(tk.Tk):
         # Add the File menu to the menubar
         menubar.add_cascade(label='Home', menu=home_menu)
         # Add menu items to the menu
-        home_menu.add_command(label='About', command=about)
+        home_menu.add_command(label='About', command=lambda: self.show_frame(AboutFrame))
         home_menu.add_separator()
         home_menu.add_command(label='Exit', command=self.destroy)
 
@@ -289,7 +290,7 @@ class App(tk.Tk):
 
         book_menu = tk.Menu(menubar, tearoff=0)
         menubar.add_cascade(label='Quản lý đầu sách', menu=book_menu)
-        book_menu.add_command(label='Quản lý sách', command=manage_book)
+        book_menu.add_command(label='Quản lý sách', command=lambda: self.show_frame(BookFrame))
         book_menu.add_separator()
         book_menu.add_command(label='Lập đơn mua sách', command=no_func)
         book_menu.add_command(label='Lập đơn hủy sách', command=no_func)
@@ -305,7 +306,11 @@ class App(tk.Tk):
         report_menu.add_command(label='Thống kê thể loại sách được mượn', command=no_func)
         report_menu.add_command(label='Thống kê sách quá hạn', command=no_func)
         report_menu.add_command(label='Thống kê độc giả', command=no_func)
-
+    
+    def show_frame(self, container):
+        frame = self.frames[container]
+        frame.tkraise()
+    
 if __name__ == '__main__':
     app = App()
     app.mainloop()
